fe727240dd255a62c0c96d3817ff1ddc16e9089c
rodrigofelicio <rodrigo.felicio@gmail.com>
2023-11-26 12:45:30 -0300

    added first API endpoint /store

--- app.py
+++ app.py
@@ -3,3 +3,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+stores = [
+    {
+        "name": "My Store",
+        "items": [
+            {
+                "name":  "chair",
+                "price": 15.99
+            }
+        ]
+    }
+]
+
+@app.get("/store")
+def get_store():
+    return {"stores": stores}
